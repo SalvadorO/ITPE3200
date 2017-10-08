38499679b76563252f97eb5e9b83f8be31808185
Salvador Orvholt <s305043@stud.hioa.no>
2017-10-08 21:48:34 +0200

    regex fix
    regex fix

--- WebApp/WebApp/Models/ViewCardInfo.cs
+++ WebApp/WebApp/Models/ViewCardInfo.cs
@@ -10,15 +10,17 @@ namespace WebApp.Models
     {
         [Display(Name = "Kortholders navn")]
         [Required(ErrorMessage = "Kortholders navn må oppgis")]
+        [RegularExpression(pattern: @"[A-Za-z\s]{3,}", ErrorMessage = "Kun bokstaver, minimum 3")]
         public String cardName { get; set; }
 
         [Display(Name = "Kortnummer")]
         [Required(ErrorMessage = "Kortnummer må oppgis")]
+        [RegularExpression(pattern: @"[0-9]{16}", ErrorMessage = "16 siffer uten mellomrom")]
         public String cardNr { get; set; }
 
         [Display(Name = "3-siffer-kode")]
         [Required(ErrorMessage = "3-siffer-kode må oppgis")]
-        [RegularExpression(@"[0-9]{3}", ErrorMessage = "Må være 3 Tall")]
+        [RegularExpression(pattern: @"[0-9]{3}", ErrorMessage = "Må være 3 Tall")]
 
         public String code { get; set; }
 
--- WebApp/WebApp/Models/ViewCustomer.cs
+++ WebApp/WebApp/Models/ViewCustomer.cs
@@ -14,18 +14,22 @@ namespace WebApp.Models
 
         [Display(Name = "Fornavn")]
         [Required(ErrorMessage = "Fornavn må oppgis")]
+        [RegularExpression(pattern: @"[A-Za-z\S]{2,}", ErrorMessage = "Kun bokstaver, minimum 3")]
         public String firstName { get; set; }
 
         [Display(Name = "Etternavn")]
         [Required(ErrorMessage = "Etternavn må oppgis")]
+        [RegularExpression(pattern: @"[A-Za-z\S]{2,}", ErrorMessage = "Kun bokstaver, minimum 3")]
         public String lastName { get; set; }
 
         [Display(Name = "Telefonnummer")]
         [Required(ErrorMessage = "Telefonnummer må oppgis")]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = "Postummer må være 8 siffer")]
         public String phoneNumber { get; set; }
 
         [Display(Name = "Adresse")]
         [Required(ErrorMessage = "Adresse må oppgis")]
+        [RegularExpression(pattern: @"[A-Za-z0-9\s\S]{4,}", ErrorMessage = "Adresse må fylles ut")]
         public String address { get; set; }
 
         [Display(Name = "Postnummer")]
@@ -35,6 +39,7 @@ namespace WebApp.Models
 
         [Display(Name = "Poststed")]
         [Required(ErrorMessage = "Poststed må oppgis")]
+        [RegularExpression(pattern: @"[A-Za-z\s]{2,}", ErrorMessage = "Kun bokstaver, minimum 3")]
         public String city { get; set; }
 
         [Display(Name = "E-Post")]
