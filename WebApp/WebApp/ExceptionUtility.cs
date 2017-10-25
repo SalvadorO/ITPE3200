6b869d48da0a29beabce80e87232fe9ec71e8384
Salvador Orvholt <s305043@stud.hioa.no>
2017-10-25 19:27:37 +0200

    Lagt til logging
    Lagt til logging av exceptions.

---
+++ WebApp/WebApp/ExceptionUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WebApp
+{
+    public sealed class ExceptionUtility
+    {
+        private ExceptionUtility()
+        { }
+
+        public static void LogException(Exception exception, string source)
+        {
+            string logfile = @"~/Errors/Error.txt";
+            logfile = HttpContext.Current.Server.MapPath(logfile);
+
+            StreamWriter sw = new StreamWriter(logfile, true);
+            sw.WriteLine("********** {0} **********", DateTime.Now);
+            if (exception.InnerException != null)
+            {
+                sw.Write("Inner Exception Type: ");
+                sw.WriteLine(exception.InnerException.GetType().ToString());
+                sw.Write("Inner Exception: ");
+                sw.WriteLine(exception.InnerException.Message);
+                sw.Write("Inner Source: ");
+                sw.WriteLine(exception.InnerException.Source);
+                if (exception.InnerException.StackTrace != null)
+                {
+                    sw.WriteLine("Inner Stack Trace: ");
+                    sw.WriteLine(exception.InnerException.StackTrace);
+                }
+            }
+            sw.Write("Exception Type: ");
+            sw.WriteLine(exception.GetType().ToString());
+            sw.WriteLine("Exception: " + exception.Message);
+            sw.WriteLine("Source: " + source);
+            sw.WriteLine("Stack Trace: ");
+            if (exception.StackTrace != null)
+            {
+                sw.WriteLine(exception.StackTrace);
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+
+     
+
+    }
+}
--- WebApp/WebApp/Global.asax.cs
+++ WebApp/WebApp/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,29 @@ namespace WebApp
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        protected void Application_Error(object sender, EventArgs e) {
+            Exception exception = Server.GetLastError();
+            
+            // Log the exception and notify system operators
+            ExceptionUtility.LogException(exception, "DefaultPage");
+           
+            Response.Write("<h2>Error!</h2>\n");
+            Response.Write(
+                "<p>" + "Exception: " + exception.Message + "</p>\n");
+            Response.Write(
+               "<p>" + "Stack Trace: "+  "</p>\n");
+            Response.Write(
+               "<p>" + exception.StackTrace + "</p>\n");
+
+
+
+
+            // Clear the error from the server
+            Server.ClearError();
+
+
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
